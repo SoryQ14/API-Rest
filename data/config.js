03675c7f093a56ea125c5da691c5c42a38e6a159
SoryQ14 <squibtanarosas64@gmail.com>
2024-04-28 14:46:48 -0600

    windows

--- data/config.js
+++ data/config.js
@@ -1,15 +1,25 @@
-const mysql = require('mysql'); 
+const sql = require('mssql');
 
-//set database connection creadentials
 const config = {
-    host: 'localhost', 
-    user: 'root', 
-    password: '12345', 
-    database: 'api',
+  user: 'sorajapi',
+  password: '12345',
+  server: 'localhost', // o la dirección del servidor
+  database: 'api',
+  options: {
+    encrypt: false, 
+    enableArithAbort: true,
+  },
 };
 
-//Create a MySQL pool 
-const pool = mysql.createPool(config); 
+const poolPromise = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    console.log('Conexión a MSSQL Server exitosa');
+    return pool;
+  })
+  .catch(err => {
+    console.error('Error al conectar a MSSQL Server:', err);
+    process.exit(1);
+  });
 
-//Export the pool
-module.exports = pool; 
+module.exports = { sql, poolPromise }
