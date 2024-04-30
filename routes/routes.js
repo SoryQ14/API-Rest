886e2b6a020fbd003c643e6607fa9ab8787e8dcc
SoryQ14 <squibtanarosas64@gmail.com>
2024-04-30 12:24:12 -0600

    encryp

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,23 @@
 const { response, request } = require('express');
 const pool =  require('../data/config'); 
 
+var sha1 = require('js-sha1');
+var { sha256, sha224 } = require('js-sha256');
+var md5 = require('md5');
+
+const encryptPasswordSHA1 = (password) => {
+    return sha1(password);
+};
+
+const encryptPasswordSHA256 = (password) => {
+    return sha256(password);
+};
+
+
+const encryptPasswordMD5 = (password) => {
+    return md5(password);
+};
+
 //Ruta de la app
 const router = app =>{
     //Mostrar mensaje de bienvenida del root 
@@ -30,6 +47,14 @@ const router = app =>{
     });
     //agregar un nuevo usuario
     app.post('/users', (request, response) => {
+        const Passwordsha256 = encryptPasswordSHA256(request.body.password);
+        const Passwordsha1 = encryptPasswordSHA1(request.body.password);
+        const Passwordmd5 = encryptPasswordMD5(request.body.password); 
+        
+        request.body.passwordSha1 = Passwordsha1;
+        request.body.passwordSha256 = Passwordsha256;
+        request.body.passwordMd5 = Passwordmd5;
+        
         pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
             if (error) throw error;
 
@@ -39,6 +64,13 @@ const router = app =>{
     //actualizar un usuario existente 
     app.put('/users/:id', (request, response) => {
         const id = request.params.id; 
+        const Passwordsha256 = encryptPasswordSHA256(request.body.password);
+        const Passwordsha1 = encryptPasswordSHA1(request.body.password);
+        const Passwordmd5 = encryptPasswordMD5(request.body.password); 
+        
+        request.body.passwordSha1 = Passwordsha1;
+        request.body.passwordSha256 = Passwordsha256;
+        request.body.passwordMd5 = Passwordmd5;
 
         pool.query('UPDATE users SET ? WHERE id = ?',[request.body, id], (error, result) =>{
             if(error) throw error;
